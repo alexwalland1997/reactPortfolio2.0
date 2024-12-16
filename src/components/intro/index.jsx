3ceb56b38fecce0950ec5c42d281e11b86c94d6c
alexwalland1997 <146854684+alexwalland1997@users.noreply.github.com>
2024-12-16 13:20:52 +0000

    intro completed
    added image and details about me

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -1,7 +1,7 @@
 export default function Intro() {
   return (
     <div id="background">
-      <div>
+      <div id="terminal">
         <p>C:/Users/alexanderwalland/</p>
         <p>> npx create-react-app Portfolio</p>
         <p>> cd portfolio</p>
@@ -10,12 +10,14 @@ export default function Intro() {
         <p>> git commit -m "created react portfolio"</p>
         <p>> git push</p>
         <p className="active">> </p>
+        </div>
         <div id="intro">
           <img src="https://media.licdn.com/dms/image/v2/D4D03AQF-E4OanYQbIA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725388912839?e=1740009600&v=beta&t=_0f_a5Eecc9a6dpCGKJjp8Ag2ca2zdUWWAh1r1KPEIk"></img>
+          <h1>Alexander Walland</h1>  
+          <h2>Front-end Programmer</h2>
+          <h2>HTML | CSS | Javascript</h2>
         </div>
-      </div>
-      <div id="layer"></div>
-      <div id="intro"></div>
+        
     </div>
   );
 }
