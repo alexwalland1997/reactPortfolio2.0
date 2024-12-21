ad9c1fa06f98ed205668cac65cf03aa1c2693d88
alexwalland1997 <146854684+alexwalland1997@users.noreply.github.com>
2024-12-21 13:43:06 +0000

    about me
    completed about me with individual container for header then content with a rainbow glow around the title

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,6 +1,38 @@
-export default function About() {
-    return <div id="about">
-        <h2 id="aboutme">About Me</h2>
+import Header from "../header";
 
-    </div>
+export default function About() {
+  return (
+    <section id="aboutme">
+      <Header heading={"About Me"} />
+      <div id="about">
+        <p>
+          Hi, I’m Alexander Walland! I'm an aspiring front-end developer
+          passionate about creating engaging user experiences. Skilled in HTML,
+          CSS, JavaScript, and React.
+        </p>
+        <p>
+          I graduated from the University of Huddersfield in 2018 with a 2:2
+          BSc(Hons) in computer games programming but upon getting over burnt
+          out I realised this wasn't what I wanted to do. Therefore I enrolled
+          in a EDX bootcamp in frontend web development in 2003 which I
+          completed in 2024.
+        </p>
+        <p>
+          Upon completing the bootcamp I had a 10 week virtual internship with
+          Prime Global where I furthered my frontend skills as well as learnt
+          how to use wordpress to further my skillset.
+        </p>
+        <p>
+          Below you will see a select sample of my projects I have created
+          across my academic time as well as my free time along with live links
+          to the site as well as my github repository's.
+        </p>
+        <p>
+          In my free time when I'm not programming I am a avid gamer who's
+          always looking at new technology to keep up with any changes as well
+          as a massive football fan.
+        </p>
+      </div>
+    </section>
+  );
 }
---
+++ src/components/header/index.jsx
@@ -0,0 +1,8 @@
+const Header = ({heading})  => {
+    return (<div id="header">
+        <h2>{heading}</h2>
+
+    </div> )
+}
+
+export default Header;
