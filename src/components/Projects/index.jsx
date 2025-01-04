b1e669b52d0123c59cf74cd738b1351e77ef0cae
alexwalland1997 <146854684+alexwalland1997@users.noreply.github.com>
2025-01-04 11:16:43 +0000

    implemented project carousel
    carousel of a selection of my projects added which can be easily changed to add new projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,12 +1,71 @@
 import Header from "../header";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Projects() {
+  const settings = {
+    dots: false,
+    lazyLoad: true,
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 1,
+    speed: 500,
+    variableWidth: true,
+  };
   return (
     <section id="projects">
       <Header heading={"My Projects"} />
       <div id="Projects">
-
+        <div className="slider-container">
+          <Slider {...settings}>
+            <div id="container">
+              <img src="https://raw.githubusercontent.com/alexwalland1997/wordleclone/refs/heads/master/image/wordle.png" />
+              <h3>Wordle Clone</h3>
+              <a href="https://alexwalland1997.github.io/wordleclone/" target="blank">
+              <button className="link">Live Link</button>
+              </a>
+              <br />
+              <a href="https://github.com/alexwalland1997/wordleclone" target="blank">
+              <button className="link">Repo Link</button>
+              </a>
+            </div>
+            <div id="container">
+              <img src="https://raw.githubusercontent.com/porlowska/career-keys/main/public/screenshots/landing_page.png" />
+              <h3>Career Keys</h3>
+              <a href="https://career-keys.netlify.app/" target="blank">
+              <button className="link">Live Link</button>
+              </a>
+              <br />
+              <a href="https://github.com/porlowska/career-keys" target="blank">
+              <button className="link">Repo Link</button>
+              </a>
+            </div>
+            <div id="container">
+              <img src="https://raw.githubusercontent.com/CodrinCarpovici/trimtutor/main/assets/images/trimtutor.png" />
+              <h3>Trim Tutor</h3>
+              <a href="https://codrincarpovici.github.io/trimtutor/" target="blank">
+              <button className="link">Live Link</button>
+              </a>
+              <br />
+              <a href="https://github.com/CodrinCarpovici/trimtutor" target="blank">
+              <button className="link">Repo Link</button>
+              </a>
+            </div>
+            <div id="container">
+              <img src="https://github.com/alexwalland1997/password-generator/raw/main/images/example.png" />
+              <h3>Password Generator</h3>
+              <a href="https://alexwalland1997.github.io/password-generator" target="blank">
+              <button className="link">Live Link</button>
+              </a>
+              <br />
+              <a href="https://github.com/alexwalland1997/password-generator" target="blank">
+              <button className="link">Repo Link</button>
+              </a>
+            </div>
+          </Slider>
+        </div>
       </div>
-      </section>
-      )
-    }
+    </section>
+  );
+}
